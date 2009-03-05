5e562f4b67e8c6d9da4d397a7098739926459c04
Yoshimasa Niwa <niw@niw.at>
2009-03-06 03:12:11 +0900

    Remove unused script files

--- opencv_build_scripts/make_framework.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin
-make -j 5 framework FRAMEWORK_ARCH=armv6 FRAMEWORK_INSTALL_PATH="@executable_path/../Frameworks"
